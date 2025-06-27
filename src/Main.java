a4219adbeaab91a456c49fcdb7bb562f3b14244f
NoobPV24977 <bobmr259@gmail.com>
2025-06-27 11:51:11 +0300

    java 3

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String sentence = sc.nextLine();
+        String[] words = sentence.trim().split("\\s+");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i > 0) sb.append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
